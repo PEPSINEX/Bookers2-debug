fbeaa0a3badfa72494827710080eaf7ce1a6ff43
EC2 Default User <obamakazuhiro@gmail.com>
2022-04-23 15:53:17 +0000

    [Fix]  フォロー・フォロワー機能：自身をフォローできないように修正

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,12 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: :User
   belongs_to :followed, class_name: :User
+
+  validate :follow_yourself
+
+  def follow_yourself
+    if follower_id == followed_id
+      errors.add(:followed, "can not follow yourself")
+    end
+  end
 end
