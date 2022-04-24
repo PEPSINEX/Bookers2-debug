48c7718af615cd926c20cfe46a3fb175670e7194
EC2 Default User <obamakazuhiro@gmail.com>
2022-04-24 02:55:07 +0000

    [Add] 非同期通信化：いいね機能について

--- app/controllers/favorite_from_user_to_books_controller.rb
+++ app/controllers/favorite_from_user_to_books_controller.rb
@@ -1,16 +1,15 @@
 class FavoriteFromUserToBooksController < ApplicationController
   def create
     user = current_user
-    book = Book.find(params[:book])
+    @book = Book.find(params[:book])
 
-    favorite = FavoriteFromUserToBook.new(user: user, book: book)
+    favorite = FavoriteFromUserToBook.new(user: user, book: @book)
     favorite.save
-    redirect_back(fallback_location: books_path)
   end
 
   def destroy
     favorite = FavoriteFromUserToBook.find(params[:id])
+    @book = favorite.book
     favorite.destroy
-    redirect_back(fallback_location: books_path)
   end
 end
